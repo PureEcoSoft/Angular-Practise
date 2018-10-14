1fa98c3a1f6720f2f0fb4710eb916eb959bc19fc
Mandar143 <mmkirad@gmail.com>
2018-10-14 13:01:05 +0530

    angular crud operations

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges } from '@angular/core';
 import { RegistrationService } from './registration.service';
+import { NgControlStatus } from '@angular/forms';
+
 
 @Component({
   selector: 'app-registration',
@@ -9,20 +11,83 @@ import { RegistrationService } from './registration.service';
 })
 export class RegistrationComponent implements OnInit {
 
+  id: string;
+  empuser: string;
+  emppass: string;
+  email: string;
+  empDetail:any;
   constructor(
     private registerService:RegistrationService
   ) { }
 
   ngOnInit() {
+    this.ngOnChanges();
+  }
+
+  ngOnChanges(){
+    
+    this.registerService.getAllemployee().subscribe(res=>{
+      console.log(res);
+      this.empDetail=res;
+    },err=>{
+      console.log(err);
+    })
+
   }
 
   onSubmit(frm:any){
     console.log(frm);
-    this.registerService.onRegister(frm).subscribe(res=>{
-      console.log(res);
-    },err=>{
-
-    });
+    if(frm._id){
+      //update
+      this.registerService.updateEmployee(frm).subscribe(res=>{
+        console.log(res);
+        alert("updated Sucessfully");
+        this.ngOnChanges();
+      },err=>{
+        console.log(err);
+      })
+    }
+    else{
+      //insert
+      this.registerService.onRegister(frm).subscribe(res=>{
+        console.log(res);
+        this.ngOnChanges();
+      },err=>{
+  
+      });
+    }
+    
   }
+
+  editRecord(id:string){
+    console.log(id);
+    this.registerService.getCurrentEmployee(id).subscribe(res=>{
+      console.log(res['result']);
+      this.id=res['result']['_id'];
+      this.email=res['result']['empEmail'];
+      this.empuser=res['result']['empUsername'];
+      this.emppass=res['result']['empPassword'];
+
+    },err=>{
+      console.log(err);
+    })
+ 
+  }
+
+  deleteRecord(id:string){
+    console.log(id);
+    if(confirm("Are You Sure Want to Delete this record?")){
+      this.registerService.deleteEmployee(id).subscribe(res=>{
+        console.log(res);
+        this.ngOnChanges();
+        alert("Record Delete Sucessfully");
+      },err=>{
+        console.log(err)
+      })
+    }
+    
+  }
+
+  
 
 }
